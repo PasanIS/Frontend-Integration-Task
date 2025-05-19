dbb2ca260fa398ba2f8ddb7abdf7da1d0c618cb4
PasanIS <pasan.isam.office1@gmail.com>
2025-05-19 09:25:19 +0530

    get-request

--- UI/asset/app.js
+++ UI/asset/app.js
@@ -7,14 +7,14 @@ let txtStudentStudentAge = document.getElementById("txt-age");
 let btnGetDetails = document.getElementById("btn-get-details");
 
 function btnGetDetailsOnClick(){
-    // Gather Text box values
-    // Generate JSON Object
+    //--- Gather Text box values
+    //--- Generate JSON Object
     let requestBody = {
         "name":txtStudentStudentName.value,
         "address":txtStudentStudentAddress.value,
         "age":txtStudentStudentAge.value
     };
-    // Initiate POST request
+    //--- Initiate POST request
     fetch("http://localhost:8080/create-student", {
         method:"POST",
         body:JSON.stringify(requestBody),
@@ -26,13 +26,38 @@ function btnGetDetailsOnClick(){
     .then(data=>{
         console.log("Response Recived");
     })
+    
+    txtStudentStudentName.value = "";
+    txtStudentStudentAddress.value = "";
+    txtStudentStudentAge.value = "";
+    fetchStudents();
+
+}
+
+function fetchStudents(){
+    fetch("http://localhost:8080/get-student")
+    .then(res => res.json())
+    .then(data => {
+        const tableBody = document.getElementById("student-table-body");
+        tableBody.innerHTML = ""; //--- Clear table before refill
+
+        data.forEach(student => {
+            const row = `<tr>
+                <td>${student.name}</td>
+                <td>${student.address}</td>
+                <td>${student.age}</td>
+            </tr>`;
+            tableBody.innerHTML += row;
+        });
+    });
 }
 
 btnGetDetails.addEventListener("click", btnGetDetailsOnClick);
 
-// Clear input fields on page load
+//--- Clear input fields on page load
 window.addEventListener("load", () => {
     txtStudentStudentName.value = "";
     txtStudentStudentAddress.value = "";
     txtStudentStudentAge.value = "";
+    fetchStudents();
 });
--- student-demo/src/main/java/edu/icet/ecom/controller/StudentController.java
+++ student-demo/src/main/java/edu/icet/ecom/controller/StudentController.java
@@ -3,13 +3,24 @@ package edu.icet.ecom.controller;
 import edu.icet.ecom.model.Student;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 @CrossOrigin
 public class StudentController {
 
+    List<Student> studentList = new ArrayList<>();
+
     @PostMapping("/create-student")
     void createStudent(@RequestBody Student student){
         System.out.println(student);
+        studentList.add(student);
+    }
+
+    @GetMapping("/get-student")
+    public List<Student> getStudent(){
+        return studentList;
     }
 }
